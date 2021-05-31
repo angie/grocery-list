5de351f2a347fe844532b16164aee646a195e970
Angie Merryweather <6738119+angie@users.noreply.github.com>
2021-05-31 09:26:52 +0100

    support adding a new item to the grocery list

--- backend/index.test.js
+++ backend/index.test.js
@@ -1,6 +1,8 @@
 import supertest from 'supertest';
 import { initApp } from './src/server';
 
+jest.mock('uuid', () => ({ v4: () => 'new-id' }));
+
 describe('backend tests', () => {
   const defaultItems = {
     1: { id: '1', label: 'cheese', isPurchased: false },
@@ -13,5 +15,19 @@ describe('backend tests', () => {
 
     const response = await request.get('/items');
     expect(response.body).toEqual(defaultItems);
+  });
+
+  it('should add a new item to the grocery list', async () => {
+    const app = initApp(defaultItems);
+    const request = supertest(app);
+
+    const response = await request
+      .post('/items')
+      .send({ label: 'washing up liquid' })
+      .set('content-type', 'application/json');
+
+    expect(response.statusCode).toBe(201);
+    expect(response.body).toEqual({ id: 'new-id', label: 'washing up liquid', isPurchased: false });
+    expect(response.headers.location).toEqual('http://localhost:3017/items/new-id');
   });
 });
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 export const initApp = (items = {}) => {
   const app = express();
@@ -9,6 +10,16 @@ export const initApp = (items = {}) => {
 
   app.get('/items', (_, res) => {
     res.send(app.locals.items);
+  });
+
+  app.post('/items', (req, res) => {
+    const id = uuidv4();
+    const { body } = req;
+    const newItem = { id, label: body.label, isPurchased: false };
+
+    app.locals.items[id] = newItem;
+
+    res.status(201).location(`http://localhost:3017/items/${id}`).send(newItem);
   });
 
   return app;
