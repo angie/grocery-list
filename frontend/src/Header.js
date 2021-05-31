728efef618b608e92ceeacf0e3fc127220176f36
Angie Merryweather <6738119+angie@users.noreply.github.com>
2021-05-31 19:11:48 +0100

    padding tweaks

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -4,7 +4,7 @@ import logo from 'url:../assets/logo.png';
 
 const Header = () => (
   <header className="container mx-auto p-4 min-w-full bg-ee-blue flex flex-row">
-    <span className="md:w-1/3">
+    <span className=" pt-6 md:w-1/3">
       <img src={logo} alt="EE logo" />
     </span>
   </header>
--- frontend/src/List.js
+++ frontend/src/List.js
@@ -5,14 +5,18 @@ import { getGroceryList } from './api/list';
 const List = () => {
   const { isLoading, isError, data } = useQuery('items', () => getGroceryList(), { retry: false });
   return (
-    <main className="container p-4">
+    <main className="container p-6 md:p-10">
       <h1 className="font-montserrat text-ee-blue text-2xl">Grocery list</h1>
 
-      {isError ? 'Error loading grocery list.' : null}
-      {isLoading ? 'Loading grocery list…' : null}
-      <ul>
-        {data?.length ? data.map((item) => <li key={item.id}>{item.label}</li>) : 'No items found'}
-      </ul>
+      <div className="mt-4">
+        {isError ? 'Error loading grocery list.' : null}
+        {isLoading ? 'Loading grocery list…' : null}
+        <ul>
+          {data?.length
+            ? data.map((item) => <li key={item.id}>{item.label}</li>)
+            : 'No items found'}
+        </ul>
+      </div>
     </main>
   );
 };
